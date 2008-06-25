1c95fe960600119befc7d4fedd0bec1f92071d3e
lenst <lenst@lysator.liu.se>
2008-06-25 11:04:22 +0200

    count-down-latch test case and improvments
    darcs-hash:20080625090422-48392-c77a324fab84471d236ce1aa00bae8bb639f4db3.gz

--- count-down-latch.lisp
+++ count-down-latch.lisp
@@ -4,8 +4,16 @@
 (in-package :net.cddr.proc)
 
 
+(defun missing-initarg (&optional keyword)
+  (if keyword
+      (error "Missing initarg ~S" keyword)
+      (error "Missing initarg")))
+
+
 (defclass count-down-latch (synchronized)
-  ((counter  :initarg :count  :accessor count-of)))
+  ((counter  :initarg :count    :accessor count-of
+             :initform (missing-initarg :count))
+   (waiting  :initform 0        :accessor waiting-of)))
 
 
 (defmethod count-down ((latch count-down-latch))
@@ -16,9 +24,29 @@
 
 (defmethod await-latch ((latch count-down-latch))
   (with-synchronization latch
+    (incf (waiting-of latch))
     (iter (until (zerop (count-of latch)))
-          (synch-locked-wait latch))))
+          (synch-locked-wait latch))
+    (unless (zerop (decf (waiting-of latch)))
+      (synch-notify latch))))
 
+
+
+(deftest count-down-latch ()
+  (let* ((n 3)
+         (latch (make-instance 'count-down-latch :count n))
+         (lock (make-lock "m"))
+         (m 0))
+    (iter (repeat n)
+          (start-process "count-down-latch test"
+                         (lambda ()
+                           (count-down latch)
+                           (await-latch latch)
+                           (with-lock lock (incf m)))))
+    (iter (repeat 10)
+          (until (= n (with-lock lock m)))
+          (sleep 0.1))
+    (should (= n m)))  )
 
 
 
